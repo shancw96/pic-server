432d097572dd00a60e60905aeb35ccdd054f07fb
shanCW <shancw1996@gmail.com>
2022-04-16 21:09:24 +0800

    feat: 增加文件上传下载功能

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,8 +1,18 @@
 import Router from "koa-router";
 import {file} from "./controller";
+import multer from '@koa/multer';
+const upload = multer({
+  storage: multer.diskStorage({
+    destination:__dirname+'/../uploads/',
+    filename:function(req,file,cb){
+      cb(null, file.originalname);
+    }
+  })
+})
 const routes = new Router();
 // FILE ROUTES
-routes.get("/public/uploads/:id", file.upload);
-routes.post("/uploads", file.download);
+// @ts-ignore
+routes.get("/public/uploads/:id", file.download);
+routes.post("/uploads", upload.any(), file.upload);
 
 export { routes };
--- src/controller/file.ts
+++ src/controller/file.ts
@@ -1,18 +1,27 @@
-import { Context } from "koa";
-
+import { BaseContext, Context } from "koa";
+import path from "path";
+import fs from 'fs';
 export default class FileController {
   /**
    * 文件下载
    */
   public static async download(ctx: Context) {
-    ctx.status = 200;
-    ctx.body = 'file get';
+    const fileName = ctx.params.id as string;
+    const file = path.join(__dirname, `/../../uploads/${fileName}`)
+    if (!fs.existsSync(file)) {
+      ctx.status = 404;
+      ctx.body="not found"
+    } else {
+      ctx.body = fs.createReadStream(file);
+    }
   }
   /**
    * 文件上传
    */
   public static async upload(ctx: Context) {
     ctx.status = 200;
-    ctx.body = 'file download';
+    console.log('ctx.files', ctx.files);
+    // @ts-ignore
+    ctx.body = 'uploads/' + ctx.request.files[0].filename;;
   }
 }
