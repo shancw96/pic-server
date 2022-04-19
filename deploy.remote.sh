d74c0006428d8d3db3717fac32f6b7269299f031
shanCW <shancw1996@gmail.com>
2022-04-19 23:37:10 +0800

    update: deploy shell

--- deploy.remote.sh
+++ deploy.remote.sh
@@ -1,10 +1,10 @@
-image_name=11.12.123.2:5000/koa-server:v1
+image_name=127.0.0.1:5000/koa-server:v1
 container_name=koa-pic-server
 volumn_path=/Users/wushangcheng/Project/koa-pic-base/uploads
 docker rm -f $container_name
 docker rmi $image_name
 docker pull $image_name
-docker run -t -p 8089:3000 \
+docker run -t -d -p 8089:3000 \
   --restart=always \
   --name=$container_name \
   -v $volumn_path:/home/node/app/uploads \
