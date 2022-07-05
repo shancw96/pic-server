39dff993ef3735c147a65404963bd5e32401a116
shanCW <shancw1996@gmail.com>
2022-07-05 18:04:16 +0800

    chore: upload shell

--- upload.sh
+++ upload.sh
@@ -3,12 +3,12 @@
 # 各类配置信息
 remote_download_path="public/uploads/"  # 下载默认的路径
 remote_upload_path="uploads/" # 上传默认的路径
-base_url="https://blog.shancw.net/" # 对象存储绑定的域名
-
+base_url="http://serial.limiaomiao.site:8089/" # 对象存储绑定的域名
+upload_url="http://serial.limiaomiao.site:8089/"
 # 上传图片
 for i in "$@"; do
     echo 'file=@'"${i}"
-    curl -s --location --request POST "$base_url""$remote_upload_path" \
+    curl -s --location --request POST "$upload_url""$remote_upload_path" \
         --header 'Content-Type: multipart/form-data' \
         --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4ifQ.AE2jPLoZsdDua3I1FfxDMCRD8KefPRLc9dcVoscmS9E' \
         -F 'file=@'"${i}"
