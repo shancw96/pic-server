7efb382828495be0a7085b4ded369b8cf16baac6
shanCW <shancw1996@gmail.com>
2022-07-06 11:24:20 +0800

    feat: imagemin 图片压缩（png，jpeg）

--- src/controller/file.ts
+++ src/controller/file.ts
@@ -1,18 +1,30 @@
 import { BaseContext, Context } from "koa";
 import path from "path";
 import fs from 'fs';
+import imagemin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminJpegtran from 'imagemin-jpegtran';
 export default class FileController {
   /**
    * 文件下载
    */
   public static async download(ctx: Context) {
     const fileName = ctx.params.id as string;
-    const file = path.join(__dirname, `/../../uploads/${fileName}`)
-    if (!fs.existsSync(file)) {
+    const filePath = path.join(__dirname, `/../../uploads/${fileName}`);
+    if (!fs.existsSync(filePath)) {
       ctx.status = 404;
-      ctx.body="not found"
+      ctx.body = "not found";
     } else {
-      ctx.body = fs.createReadStream(file);
+      const files = await imagemin([filePath], {
+        plugins: [
+          imageminJpegtran({arithmetic: true}),
+          imageminPngquant({
+            quality: [0.6, 0.8]
+          })
+        ]
+      });
+      // ctx.body = fs.createReadStream(filePath);
+      ctx.body = files[0].data;
     }
   }
   /**
@@ -22,6 +34,6 @@ export default class FileController {
     ctx.status = 200;
     console.log('ctx.files', ctx.files);
     // @ts-ignore
-    ctx.body = 'uploads/' + ctx.request.files[0].filename;;
+    ctx.body = 'uploads/' + ctx.request.files[0].filename;
   }
 }
