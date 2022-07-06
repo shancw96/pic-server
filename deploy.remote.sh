00ca2639b059b50beffbe6d01c41eaa8f6b8200f
shanCW <shancw1996@gmail.com>
2022-07-06 17:57:06 +0800

    chore: update deploy shell

--- deploy.remote.sh
+++ deploy.remote.sh
@@ -3,7 +3,7 @@ container_name=koa-pic-server
 volumn_path=/Users/wushangcheng/Project/koa-pic-base/uploads
 docker rm -f $container_name
 docker rmi $image_name
-docker pull $image_name
+docker build . -t $image_name --no-cache
 docker run -t -d -p 8089:3000 \
   --restart=always \
   --name=$container_name \
